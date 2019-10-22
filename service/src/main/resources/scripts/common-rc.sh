567a8a2e6f30cc6af72c71b6ec6a037dc2c34a5f
Valentin Titov <Titov.V.Igo@sberbank.ru>
2019-10-22 21:30:37 +0300

    jmx

--- service/src/main/resources/scripts/common-rc.sh
+++ service/src/main/resources/scripts/common-rc.sh
@@ -28,15 +28,35 @@ APPCONFIGFILE=${CONFIGDIR}/application-template.conf
 
 app_main_class_cli="com.github.vtitov.kafka.pipeline.cli.CliMain"
 
+JMX_PORT=9999
+JMX_AUTH=false # FIXME: insecure
+JMX_SSL=false
+
+JMX_ACCESS=${CONFIGDIR}/jmxremote.access
+JMX_PASS=${CONFIGDIR}/jmxremote.password
 
 LOCALRC=${BASEDIR}/conf/local-rc.sh
 [[ -r ${LOCALRC} ]] && source ${LOCALRC}
 
 #[[ -n ${bootstrap_servers} ]] && JAVA_OPTIONS="${JAVA_OPTIONS} -Dbootstrap.servers=${bootstrap_servers}"
 
+# test JMX_AUTH, pass authenticate if false
+#JAVA_OPTIONS="${JAVA_OPTIONS} -Dcom.sun.management.jmxremote.authenticate=false"
+# test JMX_AUTH, if true
+JAVA_OPTIONS="${JAVA_OPTIONS}
+  -Dcom.sun.management.jmxremote.password.file=${JMX_ACCESS}
+  -Dcom.sun.management.jmxremote.access.file=${JMX_PASS}
+"
+
+JAVA_OPTIONS="${JAVA_OPTIONS}
+  -Dcom.sun.management.jmxremote.port=${JMX_PORT}
+  -Dcom.sun.management.jmxremote.authenticate=${JMX_AUTH}
+  -Dcom.sun.management.jmxremote.ssl=${JMX_SSL}
+"
 JAVA_OPTIONS="${JAVA_OPTIONS}
   -Dlogback.configurationFile=${LOGBACKFILE}
   -Dconfig.file=${APPCONFIGFILE}
   -Dlog.dir=${LOGDIR}
 "
+
 export JAVA_OPTIONS
